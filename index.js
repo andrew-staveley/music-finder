fe4da64b36cab9272bd2080439fe1e9488fbdd2d
Andrew <andrewstaveley14@gmail.com>
2023-11-05 00:25:18 -0400

    Added functional clear button onto document. Allows for the clearing of searched items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* 
 Submit Handler:
     Takes in a search query via the search bar, sends a request to Apple's iTunes API, and
-    recieves a response with information about relevant songs to then append them to the website.
+    recieves a response with information about relevant songs to then append them to the DOM.
 */
 
 let submitHandler = function(event) {
@@ -10,16 +10,19 @@ let submitHandler = function(event) {
     fetch(`https://itunes.apple.com/search?term=${searchQuery}&entity=song&limit=10`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
+            let counter = 1
+            //console.log(data)
             for(let value of data.results) {
                 let artistCollection = document.querySelector('#artistCollection');
                 let newSongCard = document.createElement('div');
-                newSongCard.className = 'songCard';
+                newSongCard.className = `songCard`;
+                newSongCard.id = `songCard${counter}`;
 
                 //Album Image
                 let albumImageDiv = document.createElement('div');
                 let albumImage = document.createElement('img');
-                albumImageDiv.className = 'albumImage'
+                albumImageDiv.className = `albumImage`;
+                albumImageDiv.id = `albumImage${counter}`;
                 albumImage.src = `${value.artworkUrl100}`;
                 albumImage.alt = 'Album Cover';
                 albumImageDiv.appendChild(albumImage);
@@ -28,6 +31,7 @@ let submitHandler = function(event) {
                 //Track Name
                 let trackNameDiv = document.createElement('div');
                 trackNameDiv.className = 'trackName';
+                trackNameDiv.id = `trackName${counter}`;
                 let trackName = document.createElement('a');
                 trackName.href = `${value.collectionViewUrl}`;
                 trackName.innerText = `${value.trackName}`;
@@ -36,7 +40,8 @@ let submitHandler = function(event) {
 
                 //Album Name
                 let albumNameDiv = document.createElement('div');
-                albumNameDiv.className = 'albumName'
+                albumNameDiv.className = 'albumName';
+                albumNameDiv.id = `albumName${counter}`;
                 let albumName = document.createElement('a');
                 albumName.href = `${value.collectionViewUrl}`;
                 albumName.innerText = `${value.collectionName}`;
@@ -46,6 +51,7 @@ let submitHandler = function(event) {
                 //Artist Name
                 let artistNameDiv = document.createElement('div');
                 artistNameDiv.className = 'artistName';
+                artistNameDiv.id = `artistName${counter}`;
                 let artistName = document.createElement('a');
                 artistName.href = `${value.artistViewUrl}`;
                 artistName.innerText = `${value.artistName}`;
@@ -55,6 +61,7 @@ let submitHandler = function(event) {
                 //Track Genre
                 let trackGenreDiv = document.createElement('div');
                 trackGenreDiv.className = 'trackGenre';
+                trackGenreDiv.id = `trackGenre${counter}`;
                 let trackGenre = document.createElement('small');
                 trackGenre.innerText = `${value.primaryGenreName}`;
                 trackGenreDiv.appendChild(trackGenre);
@@ -63,17 +70,48 @@ let submitHandler = function(event) {
                 //Release Year
                 let releaseYearDiv = document.createElement('div');
                 releaseYearDiv.className = 'releaseYear';
+                releaseYearDiv.id = `releaseYear${counter}`;
                 let releaseYear = document.createElement('small');
                 let releaseYearRefined = value.releaseDate.slice(0, 4);
                 releaseYear.innerText = releaseYearRefined;
                 releaseYearDiv.appendChild(releaseYear);
                 newSongCard.appendChild(releaseYearDiv);
 
+                //Favorite Button
+                let favoriteDiv = document.createElement('div');
+                favoriteDiv.className = 'favoriteButton';
+                favoriteDiv.id = `favoriteButton${counter}`;
+                let favoriteButton = document.createElement('button');
+                favoriteButton.addEventListener("click", (buttonHandler))
+                favoriteButton.innerText = 'Favorite This Song!';
+                favoriteDiv.appendChild(favoriteButton);
+                newSongCard.appendChild(favoriteDiv);
+
                 //Card Push
                 artistCollection.appendChild(newSongCard);
+                
+                //Counter
+                counter++
             };
         });
 };
 
-//Submit event listener
+
+let buttonHandler = function(event) {
+    let x = this.parentNode.id
+    let y = x.slice(-1)
+    let songName = document.querySelector(`trackName${y}`)
+    let artistName = document.querySelector(`artistName${y}`)
+    console.log(songName, artistName)
+}
+
+let resetHandler = function(event) {
+    event.preventDefault();
+    let element = document.getElementById("artistCollection");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+//Event listeners
 document.querySelector('#inputForm').addEventListener("submit", (submitHandler));
+document.querySelector('#inputForm').addEventListener("reset", (resetHandler));
